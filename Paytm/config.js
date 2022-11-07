9a246ef60fab03f0d708bf6cda0f5ed11f27be9f
Sachin-75 <110448909+Sachin-75@users.noreply.github.com>
2022-11-07 15:05:05 +0530

    Update config.js

--- Paytm/config.js
+++ Paytm/config.js
@@ -1,13 +1,7 @@
 var PaytmConfig = {
-    mid: "WtIGQS69080166765096",
-    key: "3ShlskEg1yLcdycS",
+    mid: "MERCHANT KEY",
+    key: "KEY",
     website: "WEBSTAGING"
 };
 module.exports.PaytmConfig = PaytmConfig;
 
-// Test Merchant ID
-// uOkJZw18909419697439
-// Test Merchant Key
-// Df3VxSd59g5os1OF
-// Website
-// WEBSTAGING
