d4e6ddd8b91ba298056a18d5442a28f744ee7b0d
Sachin-75 <110448909+Sachin-75@users.noreply.github.com>
2022-11-07 15:31:21 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,18 +1,6 @@
 const mongoose = require("mongoose");
 
-const DB = 'mongodb+srv://Sachin-75:sachin750@cluster0.kdb6soc.mongodb.net/Spicesproject?retryWrites=true&w=majority'
+const DB = 'mongodb://localhost:27017/SpicesRegistration'
 mongoose.connect(DB).then(() => {
     console.log(`connection successfully`);
 }).catch((err) => console.log(`no connection`));
-
-
-
-// mongoose.connect("mongodb://localhost:27017/SpicesRegistration", {
-//     // useNewUrlParser:true,
-//     // useUnifiedTopology:true,
-//     // useCreateIndex:true
-// }).then(() => {
-//     console.log(`connection successful`);
-// }).catch((e) => {
-//     console.log(`no connection`);
-// })
