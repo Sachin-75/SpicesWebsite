de376e9d364b8b1813b36ef47946f541e51678e3
Sachin <sachin.kumavat93@gmail.com>
2022-11-07 11:21:47 +0530

    Initial commit

---
+++ Paytm/config.js
@@ -0,0 +1,13 @@
+var PaytmConfig = {
+    mid: "WtIGQS69080166765096",
+    key: "3ShlskEg1yLcdycS",
+    website: "WEBSTAGING"
+};
+module.exports.PaytmConfig = PaytmConfig;
+
+// Test Merchant ID
+// uOkJZw18909419697439
+// Test Merchant Key
+// Df3VxSd59g5os1OF
+// Website
+// WEBSTAGING
